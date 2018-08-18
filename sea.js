4088b9ed1f0ecd8dd052188d11349de310c2c093
Quentin Collin <quentin.collin@epitech.eu>
2018-08-18 22:39:19 +0200

    removing require for node env

--- sea.js
+++ sea.js
@@ -160,28 +160,6 @@
         TextDecoder,
         random: (len) => Buffer.from(crypto.getRandomValues(new Uint8Array(Buffer.alloc(len))))
       })
-    } else {
-      try{
-        var crypto = require('crypto');
-        const { subtle } = require('@trust/webcrypto')             // All but ECDH
-        const { TextEncoder, TextDecoder } = require('text-encoding')
-        Object.assign(api, {
-          crypto,
-          subtle,
-          TextEncoder,
-          TextDecoder,
-          random: (len) => Buffer.from(crypto.randomBytes(len))
-        });
-        try{
-          const WebCrypto = require('node-webcrypto-ossl')
-          api.ossl = new WebCrypto({directory: 'key_storage'}).subtle // ECDH
-        }catch(e){
-          console.log("node-webcrypto-ossl is optionally needed for ECDH, please install if needed.");
-        }
-      }catch(e){
-        console.log("@trust/webcrypto and text-encoding are not included by default, you must add it to your package.json!");
-        TRUST_WEBCRYPTO_OR_TEXT_ENCODING_NOT_INSTALLED;
-      }
     }
 
     module.exports = api
